20a949ef850b86373952bce5d59ffc33bdd19cbf
Dan123655 <106719160+Dan123655@users.noreply.github.com>
2022-12-05 02:47:15 +0100

    native-tw-ts-setup

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./App.{js,jsx,ts,tsx}","./app.d.{js,jsx,ts,tsx}",".expo/app.d.{js,jsx,ts,tsx}"],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
